ce0215ea4589df22b87a53f4fbd98b982ddbec31
JakubKosakowski <67673985+JakubKosakowski@users.noreply.github.com>
2020-09-07 14:25:07 +0200

    Create 1187A-Stickers and Toys.cpp
    Your favorite shop sells n Kinder Surprise chocolate eggs. You know that exactly s stickers and exactly t toys are placed in n eggs in total.
    
    Each Kinder Surprise can be one of three types:
    
    it can contain a single sticker and no toy;
    it can contain a single toy and no sticker;
    it can contain both a single sticker and a single toy.
    But you don't know which type a particular Kinder Surprise has. All eggs look identical and indistinguishable from each other.
    
    What is the minimum number of Kinder Surprise Eggs you have to buy to be sure that, whichever types they are, you'll obtain at least one sticker and at least one toy?
    
    Note that you do not open the eggs in the purchasing process, that is, you just buy some number of eggs. It's guaranteed that the answer always exists.
    
    Input
    The first line contains the single integer T (1≤T≤100) — the number of queries.
    
    Next T lines contain three integers n, s and t each (1≤n≤109, 1≤s,t≤n, s+t≥n) — the number of eggs, stickers and toys.
    
    All queries are independent.
    
    Output
    Print T integers (one number per query) — the minimum number of Kinder Surprise Eggs you have to buy to be sure that, whichever types they are, you'll obtain at least one sticker and one toy.

---
+++ 900p/1187A-Stickers and Toys.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	
+	int t;
+
+	cin >> t;
+
+	while (t--) {
+		int n, s, t;
+
+		cin >> n >> s >> t;
+
+		cout << max(n - s, n - t) + 1 << endl;
+		
+	}
+
+	return 0;
+}
