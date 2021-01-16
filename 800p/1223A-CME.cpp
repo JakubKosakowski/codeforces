49786ce2af649330db5ad45c25b1ef547f26c9b3
JakubKosakowski <67673985+JakubKosakowski@users.noreply.github.com>
2021-01-16 16:14:37 +0100

    Create 1223A-CME.cpp

---
+++ 800p/1223A-CME.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n, p = 4;
+		cin >> n;
+		if(n == 2){
+			cout<<2<<endl;
+		}
+		else{
+			if(n % 2 == 1){
+				cout<<1<<endl;
+			}
+			else{
+				cout<<0<<endl;
+			}
+		}
+	}
+	
+	return 0;
+}
